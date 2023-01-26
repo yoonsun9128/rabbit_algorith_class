76837fb2da2542b2e0a5040e8bce1b8cde2e3c59
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2023-01-26 21:17:04 +0900

    [ADD] 8번 문제 해결완료

---
+++ 1_Week/8_matter.py
@@ -0,0 +1,16 @@
+def solution(a,b):
+    result = 0
+    total_list = []
+    for i in range(a,b+1):
+        print("sss", i)
+        count = 0
+        for x in range(1,i+1):
+            if int(i) % int(x) == 0:
+                count+=1
+        if count % 2 == 0:
+            result += i
+        else:
+            result -= i
+    return result
+
+print(solution(13,17))
