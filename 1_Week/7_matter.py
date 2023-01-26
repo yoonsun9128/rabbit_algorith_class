e4255e71dbe510cd4b27ff8b637612a1604850c2
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2023-01-26 22:54:50 +0900

    [MOD] 7문제 고민 및 풀이방식

--- 1_Week/7_matter.py
+++ 1_Week/7_matter.py
@@ -1,5 +1,35 @@
 b = format(9, "b")
 c = format(30, "b")
-print(bin(9))
-print(b)
-print(c)
+# print(bin(9)) #0b1001
+# print(bin(9)[2:].zfill(5)) #1001
+# print(b) #1001
+# print(c) #11110
+
+# n	5
+# arr1	[9, 20, 28, 18, 11]
+# arr2	[30, 1, 21, 17, 28]
+
+def solution(n, arr1, arr2):
+    for a,b in arr1,arr2:
+        print(bin(a)[2:].zfill(n))
+        print(bin(b)[2:].zfill(n))
+    # return answer
+    #ValueError: too many values to unpack (expected 2)
+
+def solution2(n, arr1, arr2):
+    result = [''] * n #변의 길이
+    for i in range(len(arr1)):
+        bin1 = bin(arr1[i])[2:].zfill(n)
+        bin2 = bin(arr2[i])[2:].zfill(n)
+        print(bin1)
+        print(bin2)
+        for a in range(n):
+            print("ddd",bin1[a])
+            print("ccc",bin1[a])
+            if bin1[a] == '0' and bin2[a] =='0':
+                result[i] += ' '
+            else:
+                result[i] += '#'
+    return result
+
+print(solution2(5,[9, 20, 28, 18, 11] , [30, 1, 21, 17, 28]))
