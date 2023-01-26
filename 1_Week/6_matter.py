534d2a20803d16bfb885926c512430f2e0152c5b
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2023-01-26 20:07:10 +0900

    [MOD] 첫번째 솔루션만 통과..

--- 1_Week/6_matter.py
+++ 1_Week/6_matter.py
@@ -2,9 +2,14 @@ def solution(n, lost, reserve):
     answer = 0
     for i in range(1,n+1):
         for a in reserve:
-            if int(a)-1 < int(i) < int(a)+1 :
+            if int(a)-1 <= int(i) <= int(a)+1 :
                 answer +=1
+        for b in lost:
+            if b == i:
+                answer -=1
 
     return answer
 
 print(solution(5,[2, 4]	,[1, 3, 5]))
+print(solution(5,[2, 4],[3]))
+print(solution(3,[3],[1]))
