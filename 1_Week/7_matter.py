904c5a84f0ece35bf1cfe89c820fee88eb35df94
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2023-01-28 13:42:40 +0900

    7번 문제 수정

--- 1_Week/7_matter.py
+++ 1_Week/7_matter.py
@@ -1,5 +1,6 @@
-b = format(9, "b")
+b = format(9, "b").zfill(5)
 c = format(30, "b")
+print(b)
 # print(bin(9)) #0b1001
 # print(bin(9)[2:].zfill(5)) #1001
 # print(b) #1001
@@ -25,11 +26,15 @@ def solution2(n, arr1, arr2):
         print(bin2)
         for a in range(n):
             print("ddd",bin1[a])
-            print("ccc",bin1[a])
+            print("ccc",bin2[a])
             if bin1[a] == '0' and bin2[a] =='0':
                 result[i] += ' '
             else:
                 result[i] += '#'
     return result
 
-print(solution2(5,[9, 20, 28, 18, 11] , [30, 1, 21, 17, 28]))
+# print(solution2(5,[9, 20, 28, 18, 11] , [30, 1, 21, 17, 28]))
+print(solution2(6,[46, 33, 33 ,22, 31, 50],[27 ,56, 19, 14, 14, 10]  ))
+
+['###  #', '######', '## ###', '#  ## ', '# ####', '### # ']
+['######', '###  #', '##  ##', ' #### ', ' #####', '### # ']
